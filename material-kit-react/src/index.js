650fd2f9d7af1c47f882abfef6875a1b8d245b31
Mark Dsouza <markdsouza93@gmail.com>
2022-08-14 20:09:51 +0530

    Initial Project Added

--- material-kit-react/src/index.js
+++ material-kit-react/src/index.js
@@ -16,20 +16,20 @@ import reportWebVitals from './reportWebVitals';
 // ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let CLIENT_ID = '624972859268-hh93km802k8r3nqdq81kc9e0puesb77q.apps.googleusercontent.com';
-let API_KEY = 'AIzaSyAIFE0VPdWqZx2D6tDICfD8pZzTMWu9W7s';
 let DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
 let SCOPES = 'https://www.googleapis.com/auth/calendar.events';
+console.log("process.env.NODE_ENV",process.env.NODE_ENV)
+console.log("clientId",process.env.REACT_APP_CLIENT_ID)
+console.log("apiKey",process.env.REACT_APP_API_KEY)
 root.render(
   <HelmetProvider>
     <BrowserRouter>
       <Provider store={configureStore()}>
         <GoogleOAuthProvider
-          apiKey={API_KEY}
-          clientId={CLIENT_ID}
+          apiKey={process.env.REACT_APP_API_KEY}
+          clientId={process.env.REACT_APP_CLIENT_ID}
           discoveryDocs={DISCOVERY_DOCS}
-          scope={SCOPES}
-          // clientId="49041614094-b8f21uke9f12l1k6k3cvjtjsu9j69948.apps.googleusercontent.com"
+          scope={SCOPES}        
         >
           <App />
         </GoogleOAuthProvider>
